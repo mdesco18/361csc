fc09a728023ecc475d3bccab8247d6e8ca953d15
mdesco18 <mdesco18@uvic.ca>
2018-02-02 18:42:32 -0800

    made directories for assignments and started assignment 2 trace.py

---
+++ a2/Trace.py
@@ -0,0 +1,14 @@
+# Trace.py 
+# Author: Marc-Andre Descoteaux V00847029
+# Project: CSC361 Project 2
+# Description: track TCP trace file state information
+
+import sys
+import os
+import pcapy
+
+def main():
+
+	
+if __name__ == '__main__':
+	main()
